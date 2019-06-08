630e0a0e42b49015c984a6d283b4073e1fa5a1c5
mdoam <50501390+mdoam@users.noreply.github.com>
2019-06-08 09:12:01 -0700

    Update MainPage.xaml.cs

--- MusicPlayer-team1/MainPage.xaml.cs
+++ MusicPlayer-team1/MainPage.xaml.cs
@@ -18,17 +18,17 @@ namespace MusicPlayer_team1
             await _addMusic.AddMedia(playList, mediaPlayer);
         }
                 
-            private void ShuffleButton_Click(object sender, RoutedEventArgs e)
-            {
+        private void ShuffleButton_Click(object sender, RoutedEventArgs e)
+        {
 
-            }
+        }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             playList.Items.Remove(playList.SelectedItem);
         }
 
-            private void MediaTransportControls_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        private void MediaTransportControls_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Space)
                 e.Handled = true;
