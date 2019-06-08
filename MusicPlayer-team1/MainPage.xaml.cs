85619316f4d90d9d46cd66c6e682ca3cb301a073
mdoam <50501390+mdoam@users.noreply.github.com>
2019-06-08 09:11:09 -0700

    Update MainPage.xaml.cs

--- MusicPlayer-team1/MainPage.xaml.cs
+++ MusicPlayer-team1/MainPage.xaml.cs
@@ -23,10 +23,10 @@ namespace MusicPlayer_team1
 
             }
 
-            private void DeleteButton_Click(object sender, RoutedEventArgs e)
-            {
-
-            }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            playList.Items.Remove(playList.SelectedItem);
+        }
 
             private void MediaTransportControls_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
         {
